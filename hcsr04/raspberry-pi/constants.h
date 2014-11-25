10c513b3f08ae33f9021de65ae8ecbfcaeaccf55
Avriel Harvey <arharvey@ncsu.edu>
2014-11-25 04:51:21 +0000

    Added GPL header

--- hcsr04/raspberry-pi/constants.h
+++ hcsr04/raspberry-pi/constants.h
@@ -1,3 +1,19 @@
+#    This file is part of Turbo-sense.
+#
+#    Turbo-sense is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Turbo-sense is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Turbosense.  If not, see <http://www.gnu.org/licenses/>.
+
+
 #define TRUE      1
 #define MAX_SENSORS   10        // The number of sensors
 #define MEDIAN_READINGS  5      // The number of readings to take for the median
